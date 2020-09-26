cc4407fbce574a5d22a4b85b0b8a8d462203bc90
Mohamed K <mohamedkore@gmail.com>
2020-09-26 00:52:23 -0400

    added preset env for postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,13 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
-const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
+const postcssPreset = require('postcss-preset-env')({ stage: 0 });
 
-const purgecss = postcssPurgecss({
-      // Specify the paths to all of the template files in your project.
-      content: [
-        './public/**/*.html',
-        './src/**/*.vue',
-      ],
-});
 
 module.exports = {
     plugins: [
       tailwindcss,
       autoprefixer,
-     ...process.env.NODE_ENV === 'production'
-       ? [purgecss]
-       : [],
+      postcssPreset
     ],
 };
+
